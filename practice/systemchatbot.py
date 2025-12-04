0232ba11f0c1923c058666c4fd3d9988c9e25ae6
Abhishek Parashar <aparashar103@gmail.com>
2025-12-05 00:31:52 +0530

    chatbot and system chatbot done

---
+++ practice/systemchatbot.py
@@ -0,0 +1,28 @@
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+# System prompt (sets behavior)
+system_prompt = """
+You are a helpful Java programming assistant.
+- Answer only Java-related questions
+- Provide code examples when helpful
+- Be concise but clear
+- If question is not about Java, politely decline
+"""
+
+model = genai.GenerativeModel("gemini-2.5-flash", system_instruction=system_prompt)
+
+chat = model.start_chat(history=[])
+
+print("=== Java Assistant Chatbot ===\n")
+
+while True:
+    user_input = input("You:")
+
+    if user_input.lower=='quit':
+        break
+
+    response = chat.send_message(user_input)
+    print(f"Bot: {response.text}\n")
---
+++ practice/toppandtopk.py
@@ -0,0 +1,25 @@
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+prompt = "Complete the sentence: The robot walked into the bar and"
+print("=== Default Settings ===")
+response = model.generate_content(prompt)
+print(response.text)
+print()
+
+print("=== top_p = 0.5 (More Focused) ===")
+response = model.generate_content(prompt,generation_config=genai.types.GenerationConfig(top_p=0.5, temperature=1))
+print(response.text)
+print()
+
+print("=== top_p = 0.95 (More Diverse) ===")
+response = model.generate_content(prompt,generation_config=genai.types.GenerationConfig(top_p=0.95, temperature=1))
+print(response.text)
+print()
+
+print("=== top_k = 10 (Only top 10 words) ===")
+response = model.generate_content(prompt, generation_config=genai.types.GenerationConfig(top_k=10))
+print(response.text)
